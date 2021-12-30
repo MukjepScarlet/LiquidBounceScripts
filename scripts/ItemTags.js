e539a92584c0773c01dbd37daf46079360d05214
木葉 Scalret <93977077+MukjepScarlet@users.noreply.github.com>
2021-12-30 13:51:43 +0800

    Add EntityArrow's tags

--- scripts/ItemTags.js
+++ scripts/ItemTags.js
@@ -1,7 +1,7 @@
 script = registerScript({
     name: "ItemTags",
     authors: ["MyScarlet"],
-    version: "1.0"
+    version: "1.1"
 });
 
 script.import("Core.lib");
@@ -11,17 +11,21 @@ module = {
     name: "ItemTags",
     category: "Render",
     description: "Show NameTag for EntityItem.",
-    onRender3D: function () {
-        Java.from(mc.theWorld.loadedEntityList)
-            .filter(function(it) it instanceof EntityItem)
-            .forEach(function(entity) {
+    onRender3D: function() {
+        for each(var entity in mc.theWorld.loadedEntityList) {
+            var str;
+            if (entity instanceof EntityItem) {
                 var stack = entity.getEntityItem();
-                drawFaceToPlayer([entity.posX, entity.posY + 0.25, entity.posZ], function () {
-                    var str = stack.getDisplayName() + " *" + stack.stackSize;
-                    var width = mc.fontRendererObj.getStringWidth(str);
-                    Gui.drawRect(-width * 0.5 - 1, -mc.fontRendererObj.FONT_HEIGHT - 1, width * 0.5 + 1, 0, 0x7F000000);
-                    mc.fontRendererObj.drawString(str, -width * 0.5, -mc.fontRendererObj.FONT_HEIGHT, 0xFFFFFF);
-                });
+                str = java.lang.String.format("%s *%d", stack.getDisplayName(), stack.stackSize);
+            } else if (entity instanceof EntityArrow) {
+                str = java.lang.String.format("Arrow Of [%s]", entity.shootingEntity.getDisplayName().getUnformattedText());
+            } else continue;
+
+            drawFaceToPlayer([entity.posX, entity.posY + 0.25, entity.posZ], function() {
+                var width = mc.fontRendererObj.getStringWidth(str);
+                Gui.drawRect(-width * 0.5 - 1, -mc.fontRendererObj.FONT_HEIGHT - 1, width * 0.5 + 1, 0, 0x7F000000);
+                mc.fontRendererObj.drawStringWithShadow(str, -width * 0.5, -mc.fontRendererObj.FONT_HEIGHT, 0xFFFFFF);
             });
+        }
     }
 };
