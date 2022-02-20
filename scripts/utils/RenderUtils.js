a37df3d258852508c88edf804fcd06e652a62665
木葉 Scalret <93977077+MukjepScarlet@users.noreply.github.com>
2022-02-20 17:37:12 +0800

    Update RenderUtils.js

--- scripts/utils/RenderUtils.js
+++ scripts/utils/RenderUtils.js
@@ -10,8 +10,6 @@ var Gui = Java.type("net.minecraft.client.gui.Gui");
 
 var Color = Java.type('java.awt.Color');
 
-var RenderUtils = Java.type("net.ccbluex.liquidbounce.utils.render.RenderUtils");
-
 var colorCode = [0x000000, 0x0000AA, 0x00AA00, 0x00AAAA, 0xAA0000, 0xAA00AA, 0xFFAA00, 0xAAAAAA, 0x555555, 0x5555FF, 0x55FF55, 0x55FFFF, 0xFF5555, 0xFF55FF, 0xFFFF55, 0xFFFFFF];
 
 /**
